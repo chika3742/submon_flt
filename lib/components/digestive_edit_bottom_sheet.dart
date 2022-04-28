e57d866e44a5133fba1cef466ec0cec5b8cc93d8
chika3742 <kazu.chika.shima@gmail.com>
2022-04-28 22:31:35 +0900

    alpha7

--- lib/components/digestive_edit_bottom_sheet.dart
+++ lib/components/digestive_edit_bottom_sheet.dart
@@ -17,9 +17,12 @@ class DigestiveEditBottomSheet extends StatefulWidget {
 
 class _DigestiveEditBottomSheetState extends State<DigestiveEditBottomSheet> {
   final _acceptableMinute = [5, 10, 15, 20, 30, 45, 60];
+  final _acceptableMinutesAfter = [10, 15, 20, 30, 45, 60];
 
+  var _selection = DigestiveSelection.minutesAfter;
   var startAt = DateTime.now();
-  var minute = 10;
+  var _minute = 10;
+  var _minutesAfter = 10;
   final _controller = TextEditingController();
 
   var startAtChanged = false;
@@ -28,7 +31,7 @@ class _DigestiveEditBottomSheetState extends State<DigestiveEditBottomSheet> {
   void initState() {
     if (widget.initialData != null) {
       startAt = widget.initialData!.startAt;
-      minute = widget.initialData!.minute;
+      _minute = widget.initialData!.minute;
       _controller.text = widget.initialData!.content;
     }
     super.initState();
@@ -39,81 +42,145 @@ class _DigestiveEditBottomSheetState extends State<DigestiveEditBottomSheet> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
+        const Text("「いつから」「何分間」やりますか？"),
+        const SizedBox(height: 8),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            // date select
-            InkWell(
-              child: Text(
-                DateFormat("M/d").format(startAt),
-                style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                      color: Theme.of(context).brightness == Brightness.dark
-                          ? Colors.white
-                      : Colors.black87,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Radio<DigestiveSelection>(
+                      value: DigestiveSelection.minutesAfter,
+                      groupValue: _selection,
+                      onChanged: onRadioSelected,
+                    ),
+                    GestureDetector(
+                      child: const Text("n分後指定"),
+                      onTap: () {
+                        onRadioSelected(DigestiveSelection.minutesAfter);
+                      },
+                    ),
+                  ],
                 ),
-              ),
-              onTap: () async {
-                var result = await showDatePicker(
-                  context: context,
-                  initialDate: startAt.isBefore(DateTime.now())
-                      ? DateTime.now()
-                      : startAt,
-                  firstDate: DateTime.now(),
-                  lastDate: DateTime(2100),
-                );
-                if (result != null) {
-                  setState(() {
-                    startAt = DateTime(result.year, result.month, result.day,
-                        startAt.hour, startAt.minute);
-                    startAtChanged = true;
-                  });
-                }
-              },
+                Row(
+                  children: [
+                    Radio<DigestiveSelection>(
+                      value: DigestiveSelection.dateTime,
+                      groupValue: _selection,
+                      onChanged: onRadioSelected,
+                    ),
+                    GestureDetector(
+                      child: const Text("日時指定"),
+                      onTap: () {
+                        onRadioSelected(DigestiveSelection.dateTime);
+                      },
+                    ),
+                  ],
+                ),
+              ],
             ),
-            // time select
-            InkWell(
-              child: Text(
-                DateFormat("H:mm").format(startAt),
-                style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                    color: Theme.of(context).brightness == Brightness.dark
-                        ? Colors.white
-                        : Colors.black87),
-              ),
-              onTap: () async {
-                var result = await showTimePicker(
-                  context: context,
-                  initialTime: TimeOfDay.fromDateTime(startAt),
-                );
-                if (result != null) {
+            if (_selection == DigestiveSelection.dateTime)
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        DateFormat("M/d").format(startAt),
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.edit_calendar),
+                        splashRadius: 24,
+                        onPressed: () async {
+                          var result = await showDatePicker(
+                            context: context,
+                            initialDate: startAt.isBefore(DateTime.now())
+                                ? DateTime.now()
+                                : startAt,
+                            firstDate: DateTime.now(),
+                            lastDate: DateTime(2100),
+                          );
+                          if (result != null) {
+                            setState(() {
+                              startAt = DateTime(result.year, result.month,
+                                  result.day, startAt.hour, startAt.minute);
+                              startAtChanged = true;
+                            });
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        DateFormat("H:mm").format(startAt),
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.schedule),
+                        splashRadius: 24,
+                        onPressed: () async {
+                          var result = await showTimePicker(
+                            context: context,
+                            initialTime: TimeOfDay.fromDateTime(startAt),
+                          );
+                          if (result != null) {
+                            setState(() {
+                              startAt = DateTime(startAt.year, startAt.month,
+                                  startAt.day, result.hour, result.minute);
+                              startAtChanged = true;
+                            });
+                          }
+                        },
+                      )
+                    ],
+                  ),
+                ],
+              )
+            else
+              DropdownButton<int>(
+                value: _minutesAfter,
+                items: _acceptableMinutesAfter
+                    .map((e) => DropdownMenuItem(
+                          child: Text("$e分後",
+                              style: const TextStyle(fontSize: 20)),
+                          value: e,
+                        ))
+                    .toList(),
+                onChanged: (value) {
                   setState(() {
-                    startAt = DateTime(startAt.year, startAt.month, startAt.day,
-                        result.hour, result.minute);
-                    startAtChanged = true;
+                    _minutesAfter = value!;
                   });
-                }
-              },
-            ),
+                },
+              ),
+            const Text("から"),
             DropdownButton<int>(
-              value: minute,
+              value: _minute,
               items: _acceptableMinute
                   .map((e) => DropdownMenuItem(
-                child: Text("$e分",
-                    style: const TextStyle(
-                        fontSize: 20, fontWeight: FontWeight.bold)),
-                value: e,
-              ))
+                        child:
+                            Text("$e分間", style: const TextStyle(fontSize: 20)),
+                        value: e,
+                      ))
                   .toList(),
               onChanged: (value) {
                 setState(() {
-                  minute = value!;
+                  _minute = value!;
                 });
               },
-            )
+            ),
           ],
         ),
+        const SizedBox(height: 8),
         if ((widget.initialData == null || startAtChanged) &&
-            startAt.isBefore(DateTime.now())) const Text(
-            '現在時刻より後の時刻を選択してください', style: TextStyle(color: Colors.redAccent)),
+            startAt.isBefore(DateTime.now()) &&
+            _selection == DigestiveSelection.dateTime)
+          const Text('現在時刻より後の時刻を選択してください',
+              style: TextStyle(color: Colors.redAccent)),
         const SizedBox(height: 16),
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
@@ -135,8 +202,13 @@ class _DigestiveEditBottomSheetState extends State<DigestiveEditBottomSheet> {
               onPressed: () {
                 // 現在時刻より前に設定時、エラー表示 (編集時、時刻に変更がない場合を除く)
                 if ((widget.initialData == null || startAtChanged) &&
-                    startAt.isBefore(DateTime.now())) {
+                    startAt.isBefore(DateTime.now()) &&
+                    _selection == DigestiveSelection.dateTime) {
                   return;
+                }
+                if (_selection == DigestiveSelection.minutesAfter) {
+                  startAt =
+                      DateTime.now().add(Duration(minutes: _minutesAfter));
                 }
                 Navigator.pop(
                     context,
@@ -144,7 +216,7 @@ class _DigestiveEditBottomSheetState extends State<DigestiveEditBottomSheet> {
                       id: widget.initialData?.id,
                       submissionId: widget.submissionId,
                       startAt: startAt,
-                      minute: minute,
+                      minute: _minute,
                       content: _controller.text,
                       done: false,
                     ));
@@ -156,4 +228,15 @@ class _DigestiveEditBottomSheetState extends State<DigestiveEditBottomSheet> {
       ],
     );
   }
+
+  void onRadioSelected(DigestiveSelection? selection) {
+    setState(() {
+      _selection = selection!;
+    });
+  }
+}
+
+enum DigestiveSelection {
+  dateTime,
+  minutesAfter,
 }
