e4c6bb0ef7669704c517ecf2b61be2858b3627c2
chika3742 <kazu.chika.shima@gmail.com>
2022-05-13 20:49:08 +0900

    alpha19 ショートカット動作修正

--- lib/pages/settings/canvas_lms_sync_page.dart
+++ lib/pages/settings/canvas_lms_sync_page.dart
@@ -15,7 +15,7 @@ class CanvasLmsSyncPage extends StatefulWidget {
 }
 
 class _CanvasLmsSyncPageState extends State<CanvasLmsSyncPage> {
-  CrossFadeState _crossFadeState = CrossFadeState.showFirst;
+  bool connected = false;
   int? _currentUniversityId;
   final _accessTokenController = TextEditingController();
   final _scrollController = ScrollController();
@@ -31,7 +31,7 @@ class _CanvasLmsSyncPageState extends State<CanvasLmsSyncPage> {
         var canvas = (await FirestoreProvider.config)?.lms?.canvas;
         if (canvas != null) {
           setState(() {
-            _crossFadeState = CrossFadeState.showSecond;
+            connected = true;
             this.canvas = canvas;
           });
         }
@@ -50,183 +50,191 @@ class _CanvasLmsSyncPageState extends State<CanvasLmsSyncPage> {
       appBar: AppBar(
         title: const Text('Canvas と連携'),
       ),
-      body: AnimatedCrossFade(
-        crossFadeState: _crossFadeState,
-        duration: const Duration(milliseconds: 300),
-        firstChild: Scrollbar(
-          controller: _scrollController,
-          child: SingleChildScrollView(
-            controller: _scrollController,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                children: [
-                  const SizedBox(height: 16),
-                  Text(
-                    '大学等で利用するCanvas LMS(※)を Submon に連携することが出来ます。'
-                    '月曜日〜土曜日の18:00にCanvasに登録された提出物を同期し、Submonに自動的に追加します。\n\n'
-                    '以下から大学名を選択し、以下の方法で取得したアクセストークンを貼り付けてください。(提出物の同期以外の目的でこのアクセストークンを利用することはありません。)\n\n'
-                    '※大学によって通称が異なります(例えば岐阜大学なら「AiMS Gifu」など)。',
-                    style: Theme.of(context).textTheme.bodyLarge,
+      body: Stack(
+        children: [
+          if (!connected)
+            Scrollbar(
+              controller: _scrollController,
+              child: SingleChildScrollView(
+                controller: _scrollController,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 16),
+                      Text(
+                        '大学等で利用するCanvas LMS(※)を Submon に連携することが出来ます。'
+                        '月曜日〜土曜日の18:00にCanvasに登録された提出物を同期し、Submonに自動的に追加します。\n\n'
+                        '以下から大学名を選択し、以下の方法で取得したアクセストークンを貼り付けてください。(提出物の同期以外の目的でこのアクセストークンを利用することはありません。)\n\n'
+                        '※大学によって通称が異なります(例えば岐阜大学なら「AiMS Gifu」など)。',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      const SizedBox(height: 32),
+                      Image.asset("assets/img/canvaslms_guide_1.jpg",
+                          width: 250),
+                      const SizedBox(height: 16),
+                      Image.asset("assets/img/canvaslms_guide_2.jpg",
+                          width: 250),
+                      const SizedBox(height: 16),
+                      Image.asset("assets/img/canvaslms_guide_3.jpg",
+                          width: 250),
+                      const SizedBox(height: 16),
+                      Image.asset("assets/img/canvaslms_guide_4.jpg",
+                          width: 250),
+                      const SizedBox(height: 16),
+                      Image.asset("assets/img/canvaslms_guide_5.jpg"),
+                      const SizedBox(height: 32),
+                      DropdownButtonFormField<int>(
+                        items: UniversityLms.getDropdownItemList(),
+                        value: _currentUniversityId,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          label: Text('大学'),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _currentUniversityId = value;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 8),
+                      SizedBox(
+                        width: 200,
+                        child: OutlinedButton(
+                          child: const Text('追加報告'),
+                          onPressed: () {
+                            launchUrlString(
+                                "https://docs.google.com/forms/d/e/1FAIpQLSeb0kHMcYWkl8LDpiS6NqoViuU5DHL8FcRRBHyMXXSzhiCx3Q/viewform?usp=pp_url&entry.1540960450=Canvas+LMS%E3%82%92%E5%88%A9%E7%94%A8%E3%81%99%E3%82%8B%E5%A4%A7%E5%AD%A6%E3%81%AE%E5%A0%B1%E5%91%8A",
+                                mode: LaunchMode.externalApplication);
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        '「自分の通っている大学がない！」という際はこちらから報告してください。(ただしLMSの画面が上と同じものに限ります)',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                      const SizedBox(height: 32),
+                      TextFormField(
+                        controller: _accessTokenController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          label: Text('アクセストークン'),
+                        ),
+                      ),
+                      const SizedBox(height: 32),
+                      SizedBox(
+                        width: 250,
+                        child: ElevatedButton(
+                          child: const Text('連携する'),
+                          onPressed: integrate,
+                        ),
+                      ),
+                      const SizedBox(height: 32),
+                    ],
                   ),
-                  const SizedBox(height: 32),
-                  Image.asset("assets/img/canvaslms_guide_1.jpg", width: 250),
-                  const SizedBox(height: 16),
-                  Image.asset("assets/img/canvaslms_guide_2.jpg", width: 250),
-                  const SizedBox(height: 16),
-                  Image.asset("assets/img/canvaslms_guide_3.jpg", width: 250),
-                  const SizedBox(height: 16),
-                  Image.asset("assets/img/canvaslms_guide_4.jpg", width: 250),
-                  const SizedBox(height: 16),
-                  Image.asset("assets/img/canvaslms_guide_5.jpg"),
-                  const SizedBox(height: 32),
-                  DropdownButtonFormField<int>(
-                    items: UniversityLms.getDropdownItemList(),
-                    value: _currentUniversityId,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      label: Text('大学'),
+                ),
+              ),
+            )
+          else
+            SizedBox.expand(
+              child: SettingsListView(
+                categories: [
+                  if (canvas != null)
+                    SettingsCategory(
+                      title: "ステータス",
+                      tiles: [
+                        SettingsTile(
+                          title: "連携状態",
+                          subtitle:
+                              "連携中 (${UniversityLms.list.firstWhere((e) => e.id == canvas!.universityId, orElse: () => const UniversityLms(0, "")).name})",
+                        ),
+                        SettingsTile(
+                          title: "最終同期",
+                          subtitle: canvas!.lastSync != null
+                              ? "${DateFormat("yyyy-MM-dd HH:mm").format(canvas!.lastSync!.toDate())} (${canvas!.hasError == true ? "最後の同期に失敗しました。連携を解除し、再度連携をお試しください。" : "成功"})"
+                              : "未同期",
+                        ),
+                        SettingsTile(
+                          title: "今すぐ同期",
+                          onTap: () {
+                            showSimpleDialog(context, "注意",
+                                "本機能は短時間に何度も実行しないでください。\n\n今すぐ同期しますか？",
+                                showCancel: true, onOKPressed: () async {
+                              showLoadingModal(context);
+                              try {
+                                await FirebaseFunctions.instanceFor(
+                                        region: "asia-northeast1")
+                                    .httpsCallable("canvasSyncNow")();
+                                Navigator.pop(context);
+                                showSimpleDialog(context, "完了",
+                                    "同期リクエストを送信しました。結果は本画面でご確認ください。");
+                              } catch (e) {
+                                Navigator.pop(context);
+                                showSimpleDialog(context, "エラー", "エラーが発生しました。");
+                                print(e);
+                              }
+                            });
+                          },
+                        ),
+                        SettingsTile(
+                            title: "提出物カラー選択",
+                            subtitle: "提出物が追加される際のカラーを設定できます。",
+                            onTap: () async {
+                              var result = await showDialog<Color>(
+                                context: context,
+                                builder: (_) {
+                                  return ColorPickerDialog(
+                                      initialColor:
+                                          Color(canvas!.submissionColor));
+                                },
+                              );
+                              if (result != null) {
+                                showLoadingModal(context);
+                                await FirestoreProvider
+                                    .setCanvasLmsSubmissionColor(result);
+                                canvas?.submissionColor = result.value;
+                                Navigator.pop(context);
+                              }
+                            }),
+                        SettingsTile(
+                            title: "同期除外リストのクリア",
+                            subtitle:
+                                "提出物を削除すると自動的に除外リストに登録されます。このリストをクリアすると、次回同期時に追加されるようになります。",
+                            onTap: () {
+                              showSimpleDialog(
+                                  context, "確認", "同期除外リストをクリアしますか？",
+                                  showCancel: true, onOKPressed: () async {
+                                showLoadingModal(context);
+                                await FirestoreProvider
+                                    .clearCanvasLmsSyncExcludeList();
+                                Navigator.pop(context);
+                                showSnackBar(context, "クリアしました");
+                              });
+                            }),
+                        SettingsTile(
+                            title: "連携を解除する",
+                            onTap: () {
+                              showSimpleDialog(context, "確認",
+                                  "連携を解除しますか？\n※アクセストークンを再生成する必要があります",
+                                  showCancel: true, onOKPressed: () async {
+                                showLoadingModal(context);
+                                await FirestoreProvider.disconnectCanvasLms();
+                                Navigator.pop(context);
+                                setState(() {
+                                  connected = false;
+                                });
+                              });
+                            }),
+                        SettingsTile(
+                          subtitle: "同期は月〜土(18:00)に行われます",
+                        ),
+                      ],
                     ),
-                    onChanged: (value) {
-                      setState(() {
-                        _currentUniversityId = value;
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 8),
-                  SizedBox(
-                    width: 200,
-                    child: OutlinedButton(
-                      child: const Text('追加報告'),
-                      onPressed: () {
-                        launchUrlString(
-                            "https://docs.google.com/forms/d/e/1FAIpQLSeb0kHMcYWkl8LDpiS6NqoViuU5DHL8FcRRBHyMXXSzhiCx3Q/viewform?usp=pp_url&entry.1540960450=Canvas+LMS%E3%82%92%E5%88%A9%E7%94%A8%E3%81%99%E3%82%8B%E5%A4%A7%E5%AD%A6%E3%81%AE%E5%A0%B1%E5%91%8A",
-                            mode: LaunchMode.externalApplication);
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    '「自分の通っている大学がない！」という際はこちらから報告してください。(ただしLMSの画面が上と同じものに限ります)',
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                  const SizedBox(height: 32),
-                  TextFormField(
-                    controller: _accessTokenController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      label: Text('アクセストークン'),
-                    ),
-                  ),
-                  const SizedBox(height: 32),
-                  SizedBox(
-                    width: 250,
-                    child: ElevatedButton(
-                      child: const Text('連携する'),
-                      onPressed: integrate,
-                    ),
-                  ),
-                  const SizedBox(height: 32),
                 ],
               ),
             ),
-          ),
-        ),
-        secondChild: SizedBox.expand(
-          child: SettingsListView(
-            categories: [
-              if (canvas != null)
-                SettingsCategory(
-                  title: "ステータス",
-                  tiles: [
-                    SettingsTile(
-                      title: "連携状態",
-                      subtitle:
-                          "連携中 (${UniversityLms.list.firstWhere((e) => e.id == canvas!.universityId, orElse: () => const UniversityLms(0, "")).name})",
-                    ),
-                    SettingsTile(
-                      title: "最終同期",
-                      subtitle: canvas!.lastSync != null
-                          ? "${DateFormat("yyyy-MM-dd HH:mm").format(canvas!.lastSync!.toDate())} (${canvas!.hasError == true ? "最後の同期に失敗しました。連携を解除し、再度連携をお試しください。" : "成功"})"
-                          : "未同期",
-                    ),
-                    SettingsTile(
-                      title: "今すぐ同期",
-                      onTap: () {
-                        showSimpleDialog(context, "注意",
-                            "本機能は短時間に何度も実行しないでください。\n\n今すぐ同期しますか？",
-                            showCancel: true, onOKPressed: () async {
-                          showLoadingModal(context);
-                          try {
-                            await FirebaseFunctions.instanceFor(
-                                    region: "asia-northeast1")
-                                .httpsCallable("canvasSyncNow")();
-                            Navigator.pop(context);
-                            showSimpleDialog(context, "完了",
-                                "同期リクエストを送信しました。結果は本画面でご確認ください。");
-                          } catch (e) {
-                            Navigator.pop(context);
-                            showSimpleDialog(context, "エラー", "エラーが発生しました。");
-                            print(e);
-                          }
-                        });
-                      },
-                    ),
-                    SettingsTile(
-                        title: "提出物カラー選択",
-                        subtitle: "提出物が追加される際のカラーを設定できます。",
-                        onTap: () async {
-                          var result = await showDialog<Color>(
-                            context: context,
-                            builder: (_) {
-                              return ColorPickerDialog(
-                                  initialColor: Color(canvas!.submissionColor));
-                            },
-                          );
-                          if (result != null) {
-                            showLoadingModal(context);
-                            await FirestoreProvider.setCanvasLmsSubmissionColor(
-                                result);
-                            canvas?.submissionColor = result.value;
-                            Navigator.pop(context);
-                          }
-                        }),
-                    SettingsTile(
-                        title: "同期除外リストのクリア",
-                        subtitle:
-                            "提出物を削除すると自動的に除外リストに登録されます。このリストをクリアすると、次回同期時に追加されるようになります。",
-                        onTap: () {
-                          showSimpleDialog(context, "確認", "同期除外リストをクリアしますか？",
-                              showCancel: true, onOKPressed: () async {
-                            showLoadingModal(context);
-                            await FirestoreProvider
-                                .clearCanvasLmsSyncExcludeList();
-                            Navigator.pop(context);
-                            showSnackBar(context, "クリアしました");
-                          });
-                        }),
-                    SettingsTile(
-                        title: "連携を解除する",
-                        onTap: () {
-                          showSimpleDialog(context, "確認",
-                              "連携を解除しますか？\n※アクセストークンを再生成する必要があります",
-                              showCancel: true, onOKPressed: () async {
-                            showLoadingModal(context);
-                            await FirestoreProvider.disconnectCanvasLms();
-                            Navigator.pop(context);
-                            setState(() {
-                              _crossFadeState = CrossFadeState.showFirst;
-                            });
-                          });
-                        }),
-                    SettingsTile(
-                      subtitle: "同期は月〜土(18:00)に行われます",
-                    ),
-                  ],
-                ),
-            ],
-          ),
-        ),
+        ],
       ),
     );
   }
