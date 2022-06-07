6771e2ab18e0d4a3e4f39c84e612223ff4edaa01
chika3742 <kazu.chika.shima@gmail.com>
2022-06-07 14:40:40 +0900

    Fix: environment variables

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo $DOTENV_BASE64 > ../.env
+echo $GOOGLE_SERVICE_INFO_PLIST_BASE64 > GoogleService-Info-release.plist
+
 brew install cocoapods
 git clone https://github.com/flutter/flutter.git
 export PATH=$PATH:$(pwd)/flutter/bin
@@ -9,5 +12,3 @@ flutter precache --ios
 
 pod install
 
-echo $DOTENV_BASE64 > .env
-echo $GOOGLE_SERVICE_INFO_PLIST_BASE64 > ios/GoogleService-Info-release.plist
