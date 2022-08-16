29f44ad89667d325379e2981bf0f7aea9e0e6c7d
chika3742 <kazu.chika.shima@gmail.com>
2022-08-16 23:01:36 +0900

    メール登録ページをルートリストに追加

---
+++ lib/pages/email_registration_page.dart
@@ -0,0 +1,130 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:submon/main.dart';
+
+import '../utils/ui.dart';
+import '../utils/utils.dart';
+
+class EmailRegistrationPage extends StatefulWidget {
+  const EmailRegistrationPage({Key? key, required this.email})
+      : super(key: key);
+
+  static const routeName = "/sign-in/email/register";
+
+  final String email;
+
+  @override
+  State<EmailRegistrationPage> createState() => _EmailRegistrationPageState();
+}
+
+class EmailRegistrationPageArguments {
+  final String email;
+
+  EmailRegistrationPageArguments({required this.email});
+}
+
+class _EmailRegistrationPageState extends State<EmailRegistrationPage> {
+  final _pwController = TextEditingController();
+  String? _pwError;
+  final _pwReenterController = TextEditingController();
+  String? _pwReenterError;
+
+  bool _loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("新規登録"),
+      ),
+      body: Stack(
+        children: [
+          if (_loading) const LinearProgressIndicator(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(children: [
+              Text("メールアドレス: ${widget.email}"),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: _pwController,
+                obscureText: true,
+                enabled: !_loading,
+                decoration: InputDecoration(
+                  label: const Text("パスワード"),
+                  errorText: _pwError,
+                  border: const OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: _pwReenterController,
+                obscureText: true,
+                enabled: !_loading,
+                decoration: InputDecoration(
+                    label: const Text("パスワード(再入力)"),
+                    errorText: _pwReenterError,
+                    border: const OutlineInputBorder()),
+              ),
+            ]),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FloatingActionButton.extended(
+                label: const Text("登録"),
+                icon: const Icon(Icons.how_to_reg),
+                onPressed: register,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void register() async {
+    setState(() {
+      if (_pwController.text.isEmpty) {
+        _pwError = "入力してください";
+      } else {
+        _pwError = null;
+      }
+      if (_pwReenterController.text.isEmpty) {
+        _pwReenterError = "入力してください";
+        return;
+      } else {
+        _pwReenterError = null;
+      }
+      if (_pwReenterController.text != _pwController.text) {
+        _pwReenterError = "パスワードが一致しません";
+      } else {
+        _pwReenterError = null;
+      }
+    });
+    if (_pwError != null || _pwReenterError != null) return;
+
+    setState(() {
+      _loading = true;
+    });
+    try {
+      var result = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: widget.email, password: _pwController.text);
+
+      showSnackBar(globalContext!, "アカウントを作成しました");
+      Navigator.pop(globalContext!, result);
+    } on FirebaseAuthException catch (e, stack) {
+      setState(() {
+        _loading = false;
+      });
+      switch (e.code) {
+        case "auth/invalid-password":
+          showSnackBar(context, "パスワードが短すぎます。最低6文字で指定してください。");
+          break;
+        default:
+          handleAuthError(e, stack, context);
+          break;
+      }
+    }
+  }
+}
--- lib/pages/email_sign_in_page.dart
+++ lib/pages/email_sign_in_page.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:submon/db/shared_prefs.dart';
 import 'package:submon/main.dart';
+import 'package:submon/models/sign_in_result.dart';
+import 'package:submon/pages/email_registration_page.dart';
 import 'package:submon/utils/ui.dart';
 
 import '../utils/dynamic_links.dart';
@@ -235,10 +237,12 @@ class EmailSignInPageState extends State<EmailSignInPage>
                 }),
                 SelectSheetAction("一般的なログイン", () async {
                   Navigator.pop(context);
-                  var result = await pushPage(
-                      context, EmailRegisterPage(email: emailController.text));
-                  if (result != null) {
-                    Navigator.pop(context, result);
+                  var result = await Navigator.pushNamed<UserCredential>(
+                      context, EmailRegistrationPage.routeName,
+                      arguments: EmailRegistrationPageArguments(
+                          email: emailController.text));
+                  if (result != null && mounted) {
+                    Navigator.pop(context, SignInResult(credential: result));
                   }
                 }),
               ]);
@@ -258,7 +262,7 @@ class EmailSignInPageState extends State<EmailSignInPage>
           setState(() {
             enableEmailForm = true;
           });
-          showSimpleDialog(context, "エラー",
+          showSimpleDialog(globalContext!, "エラー",
               "このアカウントは既にGoogleログインを利用して作成されています。\n前の画面に戻り、「Google でログイン」からログインしてください。",
               onOKPressed: () {});
         }
@@ -275,8 +279,8 @@ class EmailSignInPageState extends State<EmailSignInPage>
               email: emailController.text, password: pwController.text);
         }
 
-        if (result.user != null) {
-          Navigator.of(context).pop(result);
+        if (result.user != null && mounted) {
+          Navigator.of(context).pop(SignInResult(credential: result));
         }
       }
     } on FirebaseAuthException catch (e, stack) {
@@ -367,120 +371,5 @@ class EmailSignInPageState extends State<EmailSignInPage>
         });
       }
     }, showCancel: true);
-  }
-}
-
-class EmailRegisterPage extends StatefulWidget {
-  const EmailRegisterPage({Key? key, this.email}) : super(key: key);
-
-  final String? email;
-
-  @override
-  _EmailRegisterPageState createState() => _EmailRegisterPageState();
-}
-
-class _EmailRegisterPageState extends State<EmailRegisterPage> {
-  final _pwController = TextEditingController();
-  String? _pwError = null;
-  final _pwReenterController = TextEditingController();
-  String? _pwReenterError = null;
-
-  bool _loading = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("新規登録"),
-      ),
-      body: Stack(
-        children: [
-          if (_loading) const LinearProgressIndicator(),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(children: [
-              Text("メールアドレス: ${widget.email}"),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: _pwController,
-                obscureText: true,
-                enabled: !_loading,
-                decoration: InputDecoration(
-                  label: const Text("パスワード"),
-                  errorText: _pwError,
-                  border: const OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 8),
-              TextFormField(
-                controller: _pwReenterController,
-                obscureText: true,
-                enabled: !_loading,
-                decoration: InputDecoration(
-                    label: const Text("パスワード(再入力)"),
-                    errorText: _pwReenterError,
-                    border: const OutlineInputBorder()),
-              ),
-            ]),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: FloatingActionButton.extended(
-                label: const Text("登録"),
-                icon: const Icon(Icons.how_to_reg),
-                onPressed: register,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void register() async {
-    setState(() {
-      if (_pwController.text.isEmpty) {
-        _pwError = "入力してください";
-      } else {
-        _pwError = null;
-      }
-      if (_pwReenterController.text.isEmpty) {
-        _pwReenterError = "入力してください";
-        return;
-      } else {
-        _pwReenterError = null;
-      }
-      if (_pwReenterController.text != _pwController.text) {
-        _pwReenterError = "パスワードが一致しません";
-      } else {
-        _pwReenterError = null;
-      }
-    });
-    if (_pwError != null || _pwReenterError != null) return;
-
-    setState(() {
-      _loading = true;
-    });
-    try {
-      var result = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: widget.email!, password: _pwController.text);
-
-      showSnackBar(context, "アカウントを作成しました");
-      Navigator.pop(context, result);
-    } on FirebaseAuthException catch (e, stack) {
-      setState(() {
-        _loading = false;
-      });
-      switch (e.code) {
-        case "auth/invalid-password":
-          showSnackBar(context, "パスワードが短すぎます。最低6文字で指定してください。");
-          break;
-        default:
-          handleAuthError(e, stack, context);
-          break;
-      }
-    }
   }
 }
