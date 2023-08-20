15f03e1a5a2389fd6f2f012c22d86ea37f37e846
chika3742 <43089218+chika3742@users.noreply.github.com>
2023-08-20 11:05:24 +0900

    fix: シェアから提出物作成時、リストに即座に反映されない問題の修正 (#42)
    * fix: シェアから提出物作成時、リストに即座に反映されない問題の修正
    
    * clean: 不要なasync修飾子の削除

--- lib/link_handler/opener_link_helper.dart
+++ lib/link_handler/opener_link_helper.dart
@@ -81,19 +81,26 @@ class OpenerLinkHelper {
           "期限: ${DateTime.parse(date).toLocal().toString()}\n"
           "詳細: $detail",
       showCancel: true,
-      onOKPressed: () async {
-        await SubmissionProvider().use((provider) async {
-          provider.writeTransaction(() async {
-            var id = await provider.put(Submission.from(
+      onOKPressed: () {
+        int id = 0;
+
+        SubmissionProvider().use((provider) {
+          return provider.writeTransaction(() {
+            return provider
+                .put(Submission.from(
               title: title,
               due: DateTime.parse(date).toLocal(),
               details: detail,
               color: int.parse(color),
-            ));
-            eventBus.fire(SubmissionInserted(id));
+            ))
+                .then((id_) {
+              id = id_;
+            });
           });
+        }).then((value) {
+          eventBus.fire(SubmissionInserted(id));
+          showSnackBar(globalContext!, "作成しました。");
         });
-        showSnackBar(globalContext!, "作成しました。");
       },
     );
   }
