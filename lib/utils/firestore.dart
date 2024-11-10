edd107be406ee1e52663dff50204e0bc1f9d5506
chika3742 <kazu.chika.shima@gmail.com>
2024-11-10 18:04:47 +0900

    すべてのDartコードを一旦削除

--- lib/utils/firestore.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-DocumentReference? get userDoc => FirebaseAuth.instance.currentUser != null
-    ? FirebaseFirestore.instance
-        .collection("users")
-        .doc(FirebaseAuth.instance.currentUser!.uid)
-    : null;
