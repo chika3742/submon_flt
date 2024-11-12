24f0cfc94d96c8e1e141389bfdd6dfa5b715cb72
chika3742 <kazu.chika.shima@gmail.com>
2024-11-12 12:06:47 +0900

    Androidにおける実装を整理
    - AndroidのビルドスクリプトをKotlin DSLに移行
    - Android Gradle Plugin v8.5.2に更新
    - 作り直す予定の実装を削除

---
+++ android/settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        checkNotNull(flutterSdkPath) { "flutter.sdk not found in local.properties" }
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
+rootProject.name = "submon_flt_android"
+
+include(":app")
