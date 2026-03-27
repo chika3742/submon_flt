1a5fc2887a40a3eeba29281df1bd2e50f196a37e
chika3742 <kazu.chika.shima@gmail.com>
2026-03-27 16:42:31 +0900

    Remove Stack widget in TabOthers

--- lib/pages/home_tabs/tab_others.dart
+++ lib/pages/home_tabs/tab_others.dart
@@ -23,108 +23,103 @@ class TabOthers extends ConsumerWidget {
     final isLoading = ref.watch(dataSyncServiceProvider).isLoading;
     final showReviewBtn = ref.watchPref(PrefKey.showReviewBtn);
 
-    return Stack(
-      children: [
-        Material(
-          child: SettingsListView(
-            categories: [
-              SettingsCategory(title: "Have a nice day", tiles: [
+    return Material(
+      child: SettingsListView(
+        categories: [
+          SettingsCategory(title: "Have a nice day", tiles: [
+            SettingsTile(
+              title: "今すぐ同期",
+              enabled: !isLoading,
+              leading: const Icon(Icons.sync),
+              onTap: () {
+                ref.read(dataSyncServiceProvider.notifier)
+                    .fetchData(force: true);
+              },
+            ),
+            SettingsTile(
+              title: "完了済みの提出物",
+              leading: const Icon(Icons.check),
+              onTap: () {
+                Navigator.of(context, rootNavigator: true)
+                    .pushNamed(DoneSubmissionsPage.routeName);
+              },
+            ),
+            SettingsTile(
+              title: "カスタマイズ設定",
+              leading: const Icon(Icons.auto_fix_high),
+              onTap: () {
+                Navigator.of(context, rootNavigator: true)
+                    .pushNamed(CustomizeSettingsPage.routeName);
+              },
+            ),
+            SettingsTile(
+              title: "機能設定",
+              leading: const Icon(Icons.settings),
+              onTap: () {
+                Navigator.of(context, rootNavigator: true)
+                    .pushNamed(FunctionsSettingsPage.routeName);
+              },
+            ),
+            SettingsTile(
+              title: "全般",
+              leading: const Icon(Icons.info),
+              onTap: () {
+                Navigator.of(context, rootNavigator: true)
+                    .pushNamed(GeneralSettingsPage.routeName);
+              },
+            ),
+          ]),
+          SettingsCategory(
+            title: "フィードバック・お知らせ",
+            tiles: [
+              if (showReviewBtn)
                 SettingsTile(
-                  title: "今すぐ同期",
-                  enabled: !isLoading,
-                  leading: const Icon(Icons.sync),
+                  title: "レビューを書く",
+                  subtitle: "全体的なアプリ評価はこちら。",
+                  leading: const Icon(Icons.star),
                   onTap: () {
-                    ref.read(dataSyncServiceProvider.notifier)
-                        .fetchData(force: true);
+                    Browser.openStoreListing();
                   },
                 ),
-                SettingsTile(
-                  title: "完了済みの提出物",
-                  leading: const Icon(Icons.check),
-                  onTap: () {
-                    Navigator.of(context, rootNavigator: true)
-                        .pushNamed(DoneSubmissionsPage.routeName);
-                  },
-                ),
-                SettingsTile(
-                  title: "カスタマイズ設定",
-                  leading: const Icon(Icons.auto_fix_high),
-                  onTap: () {
-                    Navigator.of(context, rootNavigator: true)
-                        .pushNamed(CustomizeSettingsPage.routeName);
-                  },
-                ),
-                SettingsTile(
-                  title: "機能設定",
-                  leading: const Icon(Icons.settings),
-                  onTap: () {
-                    Navigator.of(context, rootNavigator: true)
-                        .pushNamed(FunctionsSettingsPage.routeName);
-                  },
-                ),
-                SettingsTile(
-                  title: "全般",
-                  leading: const Icon(Icons.info),
-                  onTap: () {
-                    Navigator.of(context, rootNavigator: true)
-                        .pushNamed(GeneralSettingsPage.routeName);
-                  },
-                ),
-              ]),
-              SettingsCategory(
-                title: "フィードバック・お知らせ",
-                tiles: [
-                  if (showReviewBtn)
-                    SettingsTile(
-                      title: "レビューを書く",
-                      subtitle: "全体的なアプリ評価はこちら。",
-                      leading: const Icon(Icons.star),
-                      onTap: () {
-                        Browser.openStoreListing();
-                      },
-                    ),
-                  SettingsTile(
-                    title: "フィードバックを書く",
-                    subtitle: "不具合報告や細かな改善要望はこちら。",
-                    leading: const Icon(Icons.rate_review),
-                    onTap: () async {
-                      final deviceInfoPlugin = DeviceInfoPlugin();
-                      var deviceNameAndVer = "";
+              SettingsTile(
+                title: "フィードバックを書く",
+                subtitle: "不具合報告や細かな改善要望はこちら。",
+                leading: const Icon(Icons.rate_review),
+                onTap: () async {
+                  final deviceInfoPlugin = DeviceInfoPlugin();
+                  var deviceNameAndVer = "";
 
-                      // TODO: macOS / Windows
-                      if (Platform.isAndroid) {
-                        final info = await deviceInfoPlugin.androidInfo;
-                        deviceNameAndVer =
-                            "${info.model} / Android ${info.version.release}";
-                      } else if (Platform.isIOS) {
-                        final info = await deviceInfoPlugin.iosInfo;
-                        deviceNameAndVer =
-                            "${info.utsname.machine} / iOS ${info.systemVersion}";
-                      }
+                  // TODO: macOS / Windows
+                  if (Platform.isAndroid) {
+                    final info = await deviceInfoPlugin.androidInfo;
+                    deviceNameAndVer =
+                        "${info.model} / Android ${info.version.release}";
+                  } else if (Platform.isIOS) {
+                    final info = await deviceInfoPlugin.iosInfo;
+                    deviceNameAndVer =
+                        "${info.utsname.machine} / iOS ${info.systemVersion}";
+                  }
 
-                      final appVer = (await PackageInfo.fromPlatform()).version;
+                  final appVer = (await PackageInfo.fromPlatform()).version;
 
-                      final url =
-                          "https://docs.google.com/forms/d/e/1FAIpQLSeb0kHMcYWkl8LDpiS6NqoViuU5DHL8FcRRBHyMXXSzhiCx3Q/viewform?usp=pp_url&entry.1179597929=${Uri.encodeQueryComponent(deviceNameAndVer)}&entry.1250051436=${Uri.encodeQueryComponent(appVer)}";
-                      launchUrlString(url,
-                          mode: LaunchMode.externalApplication);
-                    },
-                  ),
-                  SettingsTile(
-                    title: "Submonに関するお知らせ",
-                    subtitle: "サービスに関するお知らせを掲載しています。",
-                    leading: const Icon(Icons.newspaper),
-                    onTap: () {
-                      Browser.openAnnouncements();
-                    },
-                  ),
-                ],
+                  final url =
+                      "https://docs.google.com/forms/d/e/1FAIpQLSeb0kHMcYWkl8LDpiS6NqoViuU5DHL8FcRRBHyMXXSzhiCx3Q/viewform?usp=pp_url&entry.1179597929=${Uri.encodeQueryComponent(deviceNameAndVer)}&entry.1250051436=${Uri.encodeQueryComponent(appVer)}";
+                  launchUrlString(url,
+                      mode: LaunchMode.externalApplication);
+                },
+              ),
+              SettingsTile(
+                title: "Submonに関するお知らせ",
+                subtitle: "サービスに関するお知らせを掲載しています。",
+                leading: const Icon(Icons.newspaper),
+                onTap: () {
+                  Browser.openAnnouncements();
+                },
               ),
             ],
           ),
-        ),
-        // HidableLinearProgressIndicator(show: isLoading),
-      ],
+        ],
+      ),
     );
   }
 }
